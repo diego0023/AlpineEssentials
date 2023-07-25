a48fec120f0584965b2c023598434e3d31f5faa6
diego0023 <diegogalan.0023@gmail.com>
2023-07-25 10:19:45 -0600

    visibility

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,41 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <title>Laravel</title>
 
-        <!-- Alpine.js-->
-         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    </head>
-    <body class="antialiased">
-        <div x-data="{ firts: 0, second: 0}">
-            <input type="text" x-model.number="firts"> + <input type="text" x-model.number="second"> = <output x-text="firts + second"></output>
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+    <!-- Alpine.js-->
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</head>
+
+<body class="antialiased">
+    <div x-data="{ currentTab: 'first'}">
+        <button @click="currentTab = 'first'">first</button>
+        <button @click="currentTab = 'second'">secnod</button>
+        <button @click="currentTab = 'third'">third</button>
+
+        <div>
+            <div x-show="currentTab === 'first'">
+                <p>Frist tab.</p>
+            </div>
+
+            <div x-show="currentTab === 'second'">
+                <p>Second tab.</p>
+            </div>
+
+            <div x-show="currentTab === 'third'">
+                <p>Third tab.</p>
+            </div>
         </div>
-    </body>
+    </div>
+
+</body>
+
 </html>
